4f823614e7c9c4440ac915691489d7014de83a94
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-07 10:24:12 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+//distanceFromHqInBlocks
+function distanceFromHqInBlocks(distance) {
+	return Math.abs(42-distance);
+}
+
+//distanceFromHqInFeet
+function distanceFromHqInFeet(distance) {
+	return Math.abs(distanceFromHqInBlocks(distance))*264;
+}
+
+//distanceTravelledInFeet
+function distanceTravelledInFeet(start,destination) {
+	return Math.abs(start-destination)*264;
+}
+
+//calculatesFarePrice
+function calculatesFarePrice(start,destination) {
+	const distance = distanceTravelledInFeet(start,destination)
+	if (distance < 400) {
+		return 0;
+	}
+	else if (distance >= 400 && distance <= 2000) {
+		return (distance - 400) * .02;
+	}
+	else if (distance > 2000 && distance < 2500) {
+		return 25;
+	}
+	else {
+		return 'cannot travel that far';
+	}
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,14 +1,18 @@
 describe('index.js', function() {
-  describe('distanceFromHqInBlocks()', function() {
-    it('returns a distance in blocks', function() {
+  describe('distanceFromHqInBlocks()',
+   function() {
+    it('returns a distance in blocks',
+     function() {
       expect(distanceFromHqInBlocks(43)).to.equal(1);
     });
 
-    it('returns a distance in blocks', function() {
+    it('returns a distance in blocks',
+     function() {
       expect(distanceFromHqInBlocks(50)).to.equal(8);
     });
 
-    it('calculates distances below 42nd street', function() {
+    it('calculates distances below 42nd street',
+     function() {
       expect(distanceFromHqInBlocks(34)).to.equal(8);
     });
   });
